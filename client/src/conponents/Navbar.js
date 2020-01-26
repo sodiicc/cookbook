9d8a71c3203e445936d13edfacc6c7b67d9f4c17
Vasiliy Tsiva <sodiicc@gmail.com>
2020-01-26 16:41:07 +0200

    fix data

--- client/src/conponents/Navbar.js
+++ client/src/conponents/Navbar.js
@@ -8,9 +8,6 @@ export const Navbar = () => {
       <Link to='/' className='navbar-brand' >Recipes Description</Link>
       <div className='collpase navbar-collapse'>
         <ul className='navbar-nav mr-auto'>
-          {/* <li className='navbar-item' >
-            <Link to='/' className='nav-link'>Recipes</Link>
-          </li> */}
           <li className='navbar-item' >
             <Link to='/create' className='nav-link'>Create Recipe Log</Link>
           </li>
